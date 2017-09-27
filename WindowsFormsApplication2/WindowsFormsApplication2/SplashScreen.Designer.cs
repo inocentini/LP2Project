8806df2b9b316623fc1f59c7470bbd138285119f
leo <IFSP-@IFSP-175798>
2017-09-27 16:50:31 -0300

    SplashScreen atualizado

--- WindowsFormsApplication2/WindowsFormsApplication2/SplashScreen.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/SplashScreen.Designer.cs
@@ -42,9 +42,10 @@
             // lbTitulo
             // 
             this.lbTitulo.AutoSize = true;
+            this.lbTitulo.BackColor = System.Drawing.Color.Transparent;
             this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbTitulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbTitulo.Location = new System.Drawing.Point(130, 32);
+            this.lbTitulo.Location = new System.Drawing.Point(125, 0);
             this.lbTitulo.Name = "lbTitulo";
             this.lbTitulo.Size = new System.Drawing.Size(418, 37);
             this.lbTitulo.TabIndex = 0;
@@ -53,6 +54,7 @@
             // lbLoad
             // 
             this.lbLoad.AutoSize = true;
+            this.lbLoad.BackColor = System.Drawing.Color.Transparent;
             this.lbLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbLoad.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.lbLoad.Location = new System.Drawing.Point(39, 154);
@@ -64,9 +66,9 @@
             // lbVersão
             // 
             this.lbVersão.AutoSize = true;
-            this.lbVersão.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lbVersão.BackColor = System.Drawing.Color.Transparent;
             this.lbVersão.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbVersão.Location = new System.Drawing.Point(513, 78);
+            this.lbVersão.Location = new System.Drawing.Point(512, 37);
             this.lbVersão.Name = "lbVersão";
             this.lbVersão.Size = new System.Drawing.Size(67, 13);
             this.lbVersão.TabIndex = 2;
@@ -74,7 +76,7 @@
             // 
             // pbFullLoad
             // 
-            this.pbFullLoad.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pbFullLoad.BackColor = System.Drawing.Color.Transparent;
             this.pbFullLoad.Location = new System.Drawing.Point(157, 154);
             this.pbFullLoad.Name = "pbFullLoad";
             this.pbFullLoad.Size = new System.Drawing.Size(410, 24);
@@ -99,6 +101,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.BackgroundImage = global::WindowsFormsApplication2.Properties.Resources.rsz_24b3b3f4d6839c2029c06ead11153598;
             this.ClientSize = new System.Drawing.Size(684, 342);
             this.Controls.Add(this.pbLoading);
             this.Controls.Add(this.pbFullLoad);
