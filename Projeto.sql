2291fb39a62015ce60dd72fb72d5699ed7ed5bb2
Gabriel Augusto Zutião <30675798+gabrielzut@users.noreply.github.com>
2017-10-02 09:17:54 -0300

    Atualizado o tamanho do campo telefone para 15

--- Projeto.sql
+++ Projeto.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE AULA;
 USE AULA;
-CREATE TABLE Pessoa(cpf varchar(14), nome varchar(64), email varchar(32), telefone varchar(14),PRIMARY KEY(cpf));
+CREATE TABLE Pessoa(cpf varchar(14), nome varchar(64), email varchar(32), telefone varchar(15),PRIMARY KEY(cpf));
