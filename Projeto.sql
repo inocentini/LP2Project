ec8649d9159762be6ed289a751f36764f6421dad
Gabriel Augusto Zutião <30675798+gabrielzut@users.noreply.github.com>
2017-10-03 12:13:04 -0300

    Adicionada a tabela (incompleta) para login

--- Projeto.sql
+++ Projeto.sql
@@ -1,3 +1,6 @@
 CREATE DATABASE AULA;
 USE AULA;
 CREATE TABLE Pessoa(cpf varchar(14), nome varchar(64), email varchar(32), telefone varchar(15),PRIMARY KEY(cpf));
+
+CREATE TABLE Login(nome varchar(16), senha varchar(256),PRIMARY KEY(nome));
+/*Falta adicionar a foreign key para ligar com pessoa*/
