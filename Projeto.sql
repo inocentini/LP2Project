1f0d5936e8ef543f443d1eadee6480d9e1ca7079
Gabriel Z <Gabriel Z@DESKTOP-UP3KUSL>
2017-10-09 12:41:23 -0300

    Adicionada a tabela "Conta"

--- Projeto.sql
+++ Projeto.sql
@@ -4,3 +4,5 @@ CREATE TABLE Pessoa(cpf varchar(14), nome varchar(64), email varchar(32), telefo
 
 CREATE TABLE Login(nome varchar(16), senha varchar(256),PRIMARY KEY(nome));
 /*Falta adicionar a foreign key para ligar com pessoa*/
+
+CREATE TABLE Conta(id int auto_increment, nome varchar(64), detalhes varchar(128), valor float, vencimento date, cpfresponsavel varchar(14),primary key(id),foreign key(cpfresponsavel) references pessoa(cpf));
