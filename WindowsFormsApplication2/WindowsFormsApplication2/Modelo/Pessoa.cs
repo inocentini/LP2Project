d279ad542db50edbcc8b79fd9a9dcee2af08a8c1
Gabriel Z <Gabriel Z@DESKTOP-UP3KUSL>
2017-11-29 13:20:28 -0200

    Feito verificadorCPF

--- WindowsFormsApplication2/WindowsFormsApplication2/Modelo/Pessoa.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Modelo/Pessoa.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace HouseManager
 {
@@ -80,6 +81,60 @@ namespace HouseManager
             {
                 telefone = value;
             }
+        }
+
+        public bool verificaCPF(string cpf)
+        {
+            cpf.Trim();
+            if (cpf.Length != 11 && System.Text.RegularExpressions.Regex.IsMatch(cpf, @"[^0-9]"))
+            {
+                return false;
+            }
+
+            string sub = cpf.Substring(0, 9);
+            int soma = 0;
+            int digito;
+
+            for(int i = 10; i >= 2; i++)
+            {
+                soma = soma + int.Parse(sub[i].ToString()) * i;
+            }
+
+            digito = soma % 11;
+
+            if(digito <= 2)
+            {
+                digito = 0;
+            }else
+            {
+                digito = 11 - digito;
+            }
+
+            if(digito != cpf.Substring(9, 1)[0])
+            {
+                return false;
+            }
+
+            sub = sub + digito;
+
+            soma = 0;
+
+            for(int i = 11; i >= 2; i++)
+            {
+                soma = soma + int.Parse(sub[i].ToString()) * i;
+            }
+
+            digito = soma % 11;
+
+            if(digito <= 2)
+            {
+                digito = 0;
+            }else
+            {
+                digito = 11 - digito;
+            }
+
+            return cpf.EndsWith(digito.ToString());
         }
     }
 }
